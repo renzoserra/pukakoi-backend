6ec325c1d3245deda808568a5b6ccbb586b0de48
renzoserra <82285676+renzoserra@users.noreply.github.com>
2025-06-28 14:22:26 -0400

    Cambios config

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import productRoutes from './routes/product.routes';
 import userRoutes from './routes/user.routes';
 import dotenv from 'dotenv';
 import { errorHandler } from './middleware/errorHandler';
+import cors from 'cors';
+
 
 dotenv.config();
 
@@ -15,6 +17,12 @@ app.use(express.json());
 app.use('/api/categories', categoryRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || '*',
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
 
 // ✔ Ruta para verificar que el backend está activo
 app.get('/api', (req, res) => {
